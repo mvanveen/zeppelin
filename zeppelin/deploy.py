b94c50319f486377b90b7577f3b39f62205a33a6
Michael Van Veen <michael@mvanveen.net>
2013-05-22 01:48:57 -0700

    v1 deploy script

--- zeppelin/deploy.py
+++ zeppelin/deploy.py
@@ -1,2 +1,18 @@
+# 1. make a temporary directory
+# 2. cp app directory over
+# 3. pip install --target -r requirements.txt build/
+# 4. check for intersection. (dry run)
+# 5.  if no intersection, move over to top-level
+# 5.1   optionally run tests
+# 6.  push up to app engine
+
+from google.appengine.tools.appcfg import AppCfgApp, StatusUpdate
+
 def main(*args):
-  print 'this is the deploy module'
+  try:
+    result = AppCfgApp(['appcfg.py', 'update'] + list(args)).Run()
+    if result:
+      sys.exit(result)
+  except KeyboardInterrupt:
+    StatusUpdate('Interrupted.')
+    sys.exit(1)
